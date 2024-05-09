c4cf0ab0cb3ce57890499dc788c71d703b9a22b7
tales emanuel <tales_emanuel@hotmail.com>
2024-05-08 22:48:15 -0300

    Endpoint /oi adicionado

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// Endpoint /oi ->ola mundo!
+app.get('/oi', function (req, res) {
+    res.send('ola, mundo')
+})
+
 app.listen(3000)
+
